a806941f27ee820b966c6aa8164f099ed62a32c3
rafaelbm91 <rafael_ata1@hotmail.com>
2018-02-08 16:44:07 -0500

    integrations of styled-components

--- __test__/App.test.tsx
+++ __test__/App.test.tsx
@@ -2,23 +2,49 @@ import * as React from 'react';
 import App from '../src/views/App';
 import * as renderer from 'react-test-renderer';
 
-test('Link changes the class when hovered', () => {
+test('Ejemplo de Test Instantaneo N° ~> ', () => {
   const component = renderer.create(
       <App />,
   );
 
   let tree  = component.toJSON();
-  expect(tree.props.className).toEqual('App');
-  expect(tree).toMatchSnapshot();
 
-  let input1 = tree.children[1].children[2];
+  /**
+   * crea evento change en input
+   */
+  tree.children[1].children[2].props.onChange({ target: { value: 'test instantaneo'} });
 
-  input1.props.onChange({ target: { value: 'test instantaneo'} });
-
+  /**
+   *  N° ~> 1
+   * test tag <a> (test contenido) </a>
+   */
   tree  = component.toJSON();
   expect(tree.children[1].children[0].children[0]).toEqual('test instantaneo');
   expect(tree).toMatchSnapshot();
+
+  /**
+   * evento click en button
+   */
+  tree.children[1].children[3].props.onClick(null);
   
-  // console.log(tree.children[1].children[0]);
+  /**
+   *  N° ~> 2
+   * test tag <ul> (test contenido) </ul>
+   */
+  tree  = component.toJSON();
+  expect(tree.children[2].children.length).toEqual(1);
+  expect(tree).toMatchSnapshot();
+
+  /**
+   *  N° ~> 3
+   * test tag <ol> (test contenido) </ol>
+   */
+  if (tree.children[2].children.length > 0) {
+    tree  = component.toJSON();
+    expect(tree.children[2].children[0].children[0]).toEqual('test instantaneo');
+    expect(tree).toMatchSnapshot();
+  }
+
+  // console.log( tree.children[2].children.length );
 
 });
--- src/views/App.tsx
+++ src/views/App.tsx
@@ -1,39 +1,96 @@
 import * as React from 'react';
-import './App.css';
+import styled from 'styled-components';
+
+import { Button } from './components/button';
 
 const logo = require('../logo.svg');
 
-class App extends React.Component<any, any> {
-  constructor (props: any) {
-    super(props);
+interface AppProps { 
+  className?: string;
+}
 
+interface AppState {
+  texto: string;
+  items: Array<string>;
+}
+
+class App extends React.Component<AppProps, AppState> {
+  
+  constructor (props: AppProps) {
+    super(props);
+    
     this.state = {
-      texto: ''
+      texto: '',
+      items: []
     };
 
     this.changeState = this.changeState.bind(this);
+    this.onclick = this.onclick.bind(this);
   }
-  changeState (event: any) {
+
+  public changeState (event: any) {
     let { value } = event.target;
     this.setState({
       texto: value
     });
   }
-  render() {
+
+  public onclick (event: any) {
+    let { texto, items } = this.state;
+    if (texto.length > 0) {
+      items.push(texto);
+      texto = '';
+      this.setState({
+        texto,
+        items
+      });
+    }
+  }
+
+  public render() {
+    let { texto, items } = this.state;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div className={this.props.className}>
+        <header>
+          <img src={logo} alt="logo" />
+          <h1>Welcome to React</h1>
         </header>
-        <p className="App-intro">
+        <p>
           <a href="#">{this.state.texto}</a>
           <br/>
-          <input type="text" onChange={this.changeState} placeholder="Texto..." />
+          <input type="text" onChange={this.changeState} value={texto} placeholder="Texto..." />
+          <Button primary={true} onClick={this.onclick}>Hola</Button>
         </p>
+        <ul>
+          {items.map( (val, id) => (<ol key={id}>{val}</ol>) )}
+        </ul>
       </div>
     );
   }
 }
 
-export default App;
+const AppComponent = styled(App)`
+  text-align: center;
+  header {
+    background-color: #222;
+    height: 150px;
+    padding: 20px;
+    color: white;
+  }
+  header > img {
+    animation: App-logo-spin infinite 20s linear;
+    height: 80px;
+  }
+  header > h1 {
+    font-size: 1.5em;
+  }
+  p {
+    font-size: large;
+  }
+  @keyframes App-logo-spin {
+    from { transform: rotate(0deg); }
+    to { transform: rotate(360deg); }
+  }
+`;
+
+export default AppComponent;
